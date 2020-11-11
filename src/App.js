aa6952a5ae730c22e7b4120d3107cfe5ebadf60d
Eduardo Melo <eduardo.melo@ivia.com.br>
2020-11-11 00:57:18 -0300

    feat - made initial layout

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'semantic-ui-css/semantic.min.css';
+import TableExampleStructured from './components';
+
+function App() {
+  return (
+    <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
+      <div style={{width: '80%'}}>
+        <TableExampleStructured/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
